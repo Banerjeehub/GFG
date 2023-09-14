e764e4957ee5f97a2183ae061b41aa7a0f8d910b
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2023-09-14 19:52:47 +0530

    Updated Solution - GfG to GitHub

--- Medium/Implementing Dijkstra Algorithm/implementing-dijkstra-algorithm.cpp
+++ Medium/Implementing Dijkstra Algorithm/implementing-dijkstra-algorithm.cpp
@@ -10,32 +10,33 @@ class Solution
     //from the source vertex S.
     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
         vector<int>dist(V, 1e9);
         dist[S] = 0;
         
+        priority_queue< pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
+        
         pq.push({0, S});
+        
         while(!pq.empty())
         {
-            int dis = pq.top().first;
+            int val = pq.top().first;
             int node = pq.top().second;
             pq.pop();
             
             for(auto it : adj[node])
             {
                 int edgeWeight = it[1];
-                int adjNode = it[0];
+                int ver = it[0];
                 
-                if(dis + edgeWeight < dist[adjNode])
+                if(val + edgeWeight < dist[ver])
                 {
-                    dist[adjNode] = dis + edgeWeight;
-                    pq.push({dis+edgeWeight, adjNode});
+                    dist[ver] = val + edgeWeight;
+                    pq.push({val + edgeWeight, ver});
                 }
             }
         }
         
         return dist;
-        
     }
 };
 
