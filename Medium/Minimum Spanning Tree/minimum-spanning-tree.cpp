dec799800bac407bb1208eb8b79500dde3e33181
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2023-09-18 20:35:47 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -7,28 +7,36 @@ using namespace std;
 class Solution
 {
 	public:
-    int spanningTree(int V, vector<vector<int>> adj[])
-    {
+    int spanningTree(int V, vector<vector<int>> adj[]){
+        
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
         
         pq.push({0, 0});
+        
         vector<int>visited(V, 0);
+        
         int resSum = 0;
         
         while(!pq.empty())
         {
             auto it = pq.top();
+            
             pq.pop();
+            
             int node = it.second;
+            
             int wt = it.first;
             
             if(visited[node]) continue;
+            
             visited[node] = 1;
+            
             resSum += wt;
             
             for(auto adjNode : adj[node])
             {
                 int tempNode = adjNode[0];
+                
                 int tempWt = adjNode[1];
                 
                 if(!visited[tempNode])
