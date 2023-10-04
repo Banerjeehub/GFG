637c3c0195340e9278d80ac9fa2583f82419e495
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2023-10-04 22:44:42 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Max and min element in Binary Tree/max-and-min-element-in-binary-tree.cpp
@@ -0,0 +1,163 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void printInorder(Node *node)
+{
+    if (node == NULL)
+    {
+        return;
+    }
+    printInorder(node->left);
+    cout << node->data << " ";
+    printInorder(node->right);
+}
+Node *buildTree(string str)
+{
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size())
+    {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N")
+        {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N")
+        {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// } Driver Code Ends
+/*
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x)
+    {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+class Solution
+{
+private:
+    void helper1(Node* root, int &maxi)
+    {
+        if(!root) return;
+        maxi = max(root->data, maxi);
+        helper1(root->left, maxi);
+        helper1(root->right, maxi);
+    }
+    void helper2(Node* root, int &mini)
+    {
+        if(!root) return;
+        mini = min(root->data, mini);
+        helper2(root->left, mini);
+        helper2(root->right, mini);
+    }
+public:
+    int findMax(Node *root)
+    {
+        int maxi = INT_MIN;
+        helper1(root, maxi);
+        return maxi;
+    }
+    int findMin(Node *root)
+    {
+        int mini = INT_MAX;
+        helper2(root, mini);
+        return mini;
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    cin.ignore();
+    while (t--)
+    {
+        string treeString;
+        getline(cin, treeString);
+        Node *root = buildTree(treeString);
+        Solution obj;
+        int mx = obj.findMax(root);
+        int mn = obj.findMin(root);
+        cout << mx << " " << mn << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
