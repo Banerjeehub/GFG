56a54ae89c2b0c7f95b5ca19c75f2c2a85126cb1
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2023-12-11 14:20:31 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
+++ Medium/Detect cycle in an undirected graph/detect-cycle-in-an-undirected-graph.cpp
@@ -3,41 +3,39 @@
 using namespace std;
 
 // } Driver Code Ends
-
-
 class Solution {
-  private:
-    bool isCyclic(int node, int parent, vector<int>adj[], vector<bool>&visited)
+  public:
+    // Function to detect cycle in an undirected graph.
+    bool dfs(int node, int parent, int *visited, vector<int>adj[])
     {
-        visited[node] = true;
+        visited[node] = 1;
         
         for(auto it : adj[node])
         {
             if(!visited[it])
             {
-                if (isCyclic(it, node, adj, visited)) return true;
+               if(dfs(it, node, visited,adj)) return true;
             }
             else if(it != parent) return true;
         }
-        
         return false;
     }
-  public:
-    // Function to detect cycle in an undirected graph.
     bool isCycle(int V, vector<int> adj[]) {
-        vector<bool>visited(V, false);
+        // Code here
+        
+        int visited[V] = {0};
         
         for(int i=0; i<V; i++)
         {
             if(!visited[i])
-                if(isCyclic(i, -1, adj, visited)) return true;
+            {
+                if(dfs(i, -1, visited, adj)) return true;
+            }
         }
         
         return false;
     }
 };
-
-
 
 //{ Driver Code Starts.
 int main() {
