a6bd037e47d81e9fe504c58e2c58950edc8dc1ba
Padmanava_Banerjee <91341613+Banerjeehub@users.noreply.github.com>
2023-12-14 14:52:53 +0530

    Updated Solution - GfG to GitHub

--- Medium/Gold Mine Problem/gold-mine-problem.cpp
+++ Medium/Gold Mine Problem/gold-mine-problem.cpp
@@ -8,29 +8,25 @@ using namespace std;
 // User function Template for C++
 class Solution{
 public:
-    int solve(int i, int j, int n, int m, vector<vector<int>> &M, vector<vector<int>> &dp){
-        if(i < 0 || i >= n || j < 0 || j >= m) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int a = solve(i-1, j+1, n, m, M, dp);
-        int b = solve(i, j+1, n, m, M, dp);
-        int c = solve(i+1, j+1, n, m, M, dp);
-        
-        return dp[i][j] = max(max(a, b), c)+M[i][j];
-    }
-    
     int maxGold(int n, int m, vector<vector<int>> M)
     {
-        int ans = 0;
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        for(int i = 0 ; i < n ; i++){
-            int a = solve(i, 0, n, m, M, dp);
-            if(a > ans) ans = a;
+        vector<vector<int>>dp(n+2, vector<int>(m+2, 0));
+        
+        for(int i = 1;i <= m;i++)
+        {
+            for(int j = 1;j <= n;j++)
+            {
+                dp[j][i] = M[j-1][i-1] + max(dp[j-1][i-1], max(dp[j][i-1], dp[j+1][i-1]));
+            }
         }
         
+       
+        int ans = INT_MIN;
+        for(int i = 0;i <= n;i++) ans = max(dp[i][m], ans);
         return ans;
     }
 };
+
 
 
 
